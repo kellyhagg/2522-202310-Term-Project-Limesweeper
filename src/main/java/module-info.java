82668f2b110931f98957a24f83b9c9ab71df8ed4
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-03-02 14:16:23 -0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example._2522_game_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example._2522_game_project to javafx.fxml;
+    exports com.example._2522_game_project;
+}
