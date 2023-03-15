ff1e2b07c84d48050e033f33d2d8acfe2b4ea346
EunjeongHur <ungdunge2@gmail.com>
2023-03-15 13:32:48 -0700

    Add StateType enum class

---
+++ src/main/java/com/example/_2522_game_project/StateType.java
@@ -0,0 +1,24 @@
+package com.example._2522_game_project;
+
+/**
+ * State type that can be used.
+ *
+ * @author Eunjeong (Alice) Hur, Kelly Hagg
+ * @version 1
+ */
+public enum StateType {
+    /**
+     * Opened state
+     */
+    OPENED,
+
+    /**
+     * Unopened state
+     */
+    UPOPENED,
+
+    /**
+     * Flagged state
+     */
+    FLAGGED
+}
