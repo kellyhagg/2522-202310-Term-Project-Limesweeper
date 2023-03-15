2f91ac0881693892f25a101f0b367d078977b085
EunjeongHur <ungdunge2@gmail.com>
2023-03-15 13:59:46 -0700

    Create Board.java class and add constructors

---
+++ src/main/java/com/example/_2522_game_project/Board.java
@@ -0,0 +1,40 @@
+package com.example._2522_game_project;
+
+/**
+ * @author Eunjeong (Alice) Hur, Kelly Hagg
+ * @version 1
+ */
+public class Board {
+    private int row;
+    private int col;
+    private Cell[][] board;
+    private int numLimes;
+
+    public Board(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.board = new Cell[row][col];
+
+        int totalCells = row * col;
+        /*TODO: Check if totalCells is less than 225 (15 * 15) and greater than 64 (8*8). Otherwise, create an exception. ( Can be other number) */
+        if (totalCells <= 100) {
+            this.numLimes = 10;
+        } else if ( totalCells <= 150) {
+            this.numLimes = 12;
+        } else if (totalCells <= 200) {
+            this.numLimes = 13;
+        } else {
+            this.numLimes = 15;
+        }
+    }
+
+    public Board(int row, int col, int minLimes) {
+        this.row = row;
+        this.col = col;
+        this.board = new Cell[row][col];
+
+        int totalCells = row * col;
+        /*TODO: if minLimes is greater than or equal to totalCells, create an exception. */
+        this.numLimes = minLimes;
+    }
+}
