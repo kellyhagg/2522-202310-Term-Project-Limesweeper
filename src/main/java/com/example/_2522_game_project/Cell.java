2110d03a981aa988fbf5f978f33a6339038d16cb
EunjeongHur <ungdunge2@gmail.com>
2023-03-15 13:37:01 -0700

    Add a Cell constructor to Cell.java class

--- src/main/java/com/example/_2522_game_project/Cell.java
+++ src/main/java/com/example/_2522_game_project/Cell.java
@@ -5,4 +5,12 @@ package com.example._2522_game_project;
  * @version 1
  */
 public class Cell {
+    private StateType state;
+    private boolean isLime;
+    private int  neighbourLimes;
+
+    public Cell(StateType state, boolean isLime) {
+        this.state = state;
+        this.isLime = isLime;
+    }
 }
