6d864c5e69bc8ac9a0d3e811913b7a2d1d2eeef0
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-03-16 13:22:39 -0700

    Modify scene size and refactor file names

--- src/main/java/com/example/_2522_game_project/LimesweeperApplication.java
+++ src/main/java/com/example/_2522_game_project/LimesweeperApplication.java
@@ -1,6 +1,10 @@
 package com.example._2522_game_project;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -8,9 +12,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LimesweeperApplication extends Application {
+    Button button;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LimesweeperApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LimesweeperApplication.class.getResource("hard-minesweeper.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 680, 600);
         stage.setTitle("Limesweeper");
         stage.setScene(scene);
