801543bec2ac52d55a7d5797285f7a8c7f31d2fd
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-03-17 19:04:46 -0700

    Add button-to-image functionality

--- src/main/java/com/example/_2522_game_project/Controller.java
+++ src/main/java/com/example/_2522_game_project/Controller.java
@@ -3,16 +3,20 @@ package com.example._2522_game_project;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.Scene;
-import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
 
 public class Controller {
 
     @FXML
-    protected void onButtonClick(ActionEvent event) {
+    protected void onButtonClick(ActionEvent event) throws IOException {
+        Image lime = new Image(LimesweeperApplication.class.getResource("lime.png").openStream());
+        ImageView limeView = new ImageView(lime);
         Button btn = (Button) event.getSource();
         String id = btn.getId();
-        btn.setText("x");
+        btn.setText("x"); // how to set the clicked button to another text
+//        btn.setGraphic(limeView); // how an image is loaded in the cell (I need to adjust the size down)
     }
 }
