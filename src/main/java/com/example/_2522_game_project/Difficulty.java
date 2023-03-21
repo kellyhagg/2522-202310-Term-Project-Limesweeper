26d14a2db216dcda3d4f34254d9eee7675da27c2
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-03-21 16:37:02 -0700

    Create difficulty enum for board difficulty

---
+++ src/main/java/com/example/_2522_game_project/Difficulty.java
@@ -0,0 +1,25 @@
+package com.example._2522_game_project;
+
+/**
+ * Board difficulties.
+ *
+ * @author Eunjeong (Alice) Hur, Kelly Hagg
+ * @version 1
+ */
+public enum Difficulty {
+    /**
+     * Easy difficulty (9 W x 9 H board)
+     */
+    EASY,
+
+    /**
+     * Medium difficulty (16 W x 16 H board)
+     */
+    MEDIUM,
+
+    /**
+     * Hard difficulty (30 W x 16 H board)
+     */
+    HARD
+}
+}
