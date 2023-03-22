fec00e6c6b4ac1de317caf0a3a79ae3224f37108
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-03-21 17:45:54 -0700

    Create content pane logic updated and style set

--- src/main/java/com/example/_2522_game_project/LimesweeperApplication.java
+++ src/main/java/com/example/_2522_game_project/LimesweeperApplication.java
@@ -1,35 +1,31 @@
 package com.example._2522_game_project;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class LimesweeperApplication extends Application {
 
-    public static final int CELL_SIZE = 24;
-
-    private Pane createPane() {
+    private Pane createContentPane() {
         Pane pane = new Pane();
-        pane.setPrefSize(720, 384);
-
+        pane.setPrefSize(810, 432); // hardcoded for now, Kelly to make dynamic with each difficulty
+        pane.setStyle("-fx-background-color: green;");
+        Board board = new Board(30, 16); // hardcoded here for now (likely to move)
+        Cell[][] boardGrid = board.getBoardGrid();
+        for (int columns = 0; columns < board.getRows(); columns++) {
+            for (int rows = 0; rows < board.getColumns(); rows++) {
+                pane.getChildren().add(boardGrid[columns][rows]);
+            }
+        }
         return pane;
+
+
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(LimesweeperApplication.class.getResource("hard-limesweeper.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 740, 450);
-        Scene scene = new Scene(createPane());
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(createContentPane());
         stage.setTitle("Limesweeper");
         stage.setScene(scene);
         stage.show();
