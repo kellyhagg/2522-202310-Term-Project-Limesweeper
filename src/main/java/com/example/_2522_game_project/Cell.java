260c2ba8b4ef37a69d751cf5bb6532cfe9cee860
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-03-21 17:43:45 -0700

    Add instance variables and constructor with GUI Pane logic

--- src/main/java/com/example/_2522_game_project/Cell.java
+++ src/main/java/com/example/_2522_game_project/Cell.java
@@ -1,4 +1,32 @@
 package com.example._2522_game_project;
 
-public class Cell {
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Cell extends StackPane {
+    public static final int CELL_SIZE = 24;
+    public static final int PANE_SIZE = 27;
+    public Rectangle outline = new Rectangle(CELL_SIZE, CELL_SIZE);
+    private final int row;
+    private final int column;
+    private final boolean isLime;
+
+    private StateType state;
+
+    public Cell(int row, int column) {
+        this.row = row;
+        this.column = column;
+        this.isLime = false;
+        this.state = StateType.UNOPENED;
+
+        outline.setStroke(Color.LIGHTGREEN);
+
+        getChildren().addAll(outline);
+
+        setTranslateX(row * PANE_SIZE);
+        setTranslateY(column * PANE_SIZE);
+    }
+
+
 }
