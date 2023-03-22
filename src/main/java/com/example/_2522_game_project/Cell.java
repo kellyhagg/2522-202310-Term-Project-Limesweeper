1d57b2ff62b7140ecdd411bd2d1dea13bc178886
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-03-21 18:18:53 -0700

    Clean up Cell constructor and add isLime accessor

--- src/main/java/com/example/_2522_game_project/Cell.java
+++ src/main/java/com/example/_2522_game_project/Cell.java
@@ -4,29 +4,47 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * The cell in a board.
+ *
+ * @author Eunjeong (Alice) Hur, Kelly Hagg
+ * @version 230321
+ */
 public class Cell extends StackPane {
     public static final int CELL_SIZE = 24;
     public static final int PANE_SIZE = 27;
-    public Rectangle outline = new Rectangle(CELL_SIZE, CELL_SIZE);
+    public static final Rectangle outline = new Rectangle(CELL_SIZE, CELL_SIZE);
     private final int row;
     private final int column;
-    private final boolean isLime;
-
+    private boolean isLime;
     private StateType state;
 
-    public Cell(int row, int column) {
+    public Cell(int column, int row) {
         this.row = row;
         this.column = column;
         this.isLime = false;
         this.state = StateType.UNOPENED;
 
+        // Set the rectangle outline to green and add to each cell
         outline.setStroke(Color.LIGHTGREEN);
-
         getChildren().addAll(outline);
-
-        setTranslateX(row * PANE_SIZE);
-        setTranslateY(column * PANE_SIZE);
+        setTranslateY(row * PANE_SIZE + 1);
+        setTranslateX(column * PANE_SIZE + 1);
     }
 
+    public boolean isLime() {
+        return isLime;
+    }
 
+    public void setLime(boolean lime) {
+        isLime = lime;
+    }
+
+    public StateType getState() {
+        return state;
+    }
+
+    public void setState(StateType state) {
+        this.state = state;
+    }
 }
