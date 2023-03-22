055c3816e28c09ce98e77dbfe813b0bbe1263ef5
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-03-21 17:33:02 -0700

    Add accessors and update Board constructor

--- src/main/java/com/example/_2522_game_project/Board.java
+++ src/main/java/com/example/_2522_game_project/Board.java
@@ -1,9 +1,37 @@
 package com.example._2522_game_project;
 
 public class Board {
-    private int rows;
-    private int columns;
-
-    private Cell[][] board;
+    public static final int HARD_COLUMNS = 30;
+    public static final int HARD_ROWS = 16;
+    private final int rows;
+    private final int columns;
+    private final Cell[][] boardGrid = new Cell[HARD_COLUMNS][HARD_ROWS];
     private int numLimes;
+    public int EASY_ROWS = 9;
+    public int EASY_COLUMNS = 9;
+    public int MEDIUM_ROWS = 9;
+    public int MEDIUM_COLUMNS = 9;
+
+    public Board(int columns, int rows) {
+        this.rows = columns;
+        this.columns = rows;
+        for (int column = 0; column < columns; column++) {
+            for (int row = 0; row < rows; row++) {
+                Cell cell = new Cell(column, row);
+                boardGrid[column][row] = cell;
+            }
+        }
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public Cell[][] getBoardGrid() {
+        return boardGrid;
+    }
 }
