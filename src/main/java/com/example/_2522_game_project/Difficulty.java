c017e6014c965e8e85366e76a54b391a63685495
Kelly Hagg <112842439+kellyhagg@users.noreply.github.com>
2023-04-01 20:35:24 -0700

    Create difficulty enum

---
+++ src/main/java/com/example/_2522_game_project/Difficulty.java
@@ -0,0 +1,24 @@
+package com.example._2522_game_project;
+
+/**
+ * State type that can be used.
+ *
+ * @author Eunjeong (Alice) Hur, Kelly Hagg
+ * @version 230401
+ */
+public enum Difficulty {
+    /**
+     * Easy difficulty
+     */
+    EASY,
+
+    /**
+     * Medium difficulty
+     */
+    MEDIUM,
+
+    /**
+     * Hard difficulty
+     */
+    HARD,
+}
