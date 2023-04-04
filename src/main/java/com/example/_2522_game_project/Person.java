7870dfc5899815f54fdede487bb179eecaa4239f
EunjeongHur <ungdunge2@gmail.com>
2023-04-04 12:14:37 -0700

    Modify readFile() code and sort the People list containing Person classes in the showLeaderBoard() method

---
+++ src/main/java/com/example/_2522_game_project/Person.java
@@ -0,0 +1,19 @@
+package com.example._2522_game_project;
+
+public class Person {
+    private String name;
+    private int score;
+
+    public Person(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
